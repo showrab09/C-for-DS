306503dcf600ec8b1e183c4fb3338b8ca446213e
showrab09 <showrabbinhabib@gmail.com>
2024-05-20 21:48:08 +0530

    Learning basics

---
+++ input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+int n,p;
+cin>>n>>p;
+
+cout<<"My age is "<<n <<endl <<p;
+return 0;
+}
---
+++ output.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+    // std::cout<<"Hello";
+
+    int n=100;
+
+    std:: cout<<"I have "<<n <<" Taka" <<std::endl<<"My name is Showrab" ;
+
+    return 0;
+}
